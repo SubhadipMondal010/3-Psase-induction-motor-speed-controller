68d6598e7799f9e1c976d0a2cd6f1d261f671a67
Subhadip Mondal <subhadipmondal11november@gmail.com>
2024-11-26 21:29:44 +0530

    Update VFD Code.ino

--- VFD Code.ino
+++ VFD Code.ino
@@ -1,9 +1,9 @@
 const int potPin = A0; // Pin for the potentiometer
 const int mosfetA = 2; // High-side MOSFET A
-const int mosfetB = 3; // Low-side MOSFET A'
-const int mosfetC = 4; // High-side MOSFET B
-const int mosfetD = 5; // Low-side MOSFET B'
-const int mosfetE = 6; // High-side MOSFET C
+const int mosfetB = 3; // High-side MOSFET B
+const int mosfetC = 4; // High-side MOSFET C
+const int mosfetD = 5; // Low-side MOSFET A'
+const int mosfetE = 6; // Low-side MOSFET B'
 const int mosfetF = 7; // Low-side MOSFET C'
 
 // Frequency range in Hertz
@@ -51,31 +51,31 @@ void cyclePhases() {
   switch (step) {
     case 0:
       digitalWrite(mosfetA, HIGH);
-      digitalWrite(mosfetB, LOW);
-      delay(Time);  
-      break;
-    case 1: 
-      digitalWrite(mosfetC, HIGH);
       digitalWrite(mosfetD, LOW);
       delay(Time);  
       break;
+    case 1: 
+      digitalWrite(mosfetB, HIGH);
+      digitalWrite(mosfetE, LOW);
+      delay(Time);  
+      break;
     case 2: 
-      digitalWrite(mosfetE, HIGH);
+      digitalWrite(mosfetC, HIGH);
       digitalWrite(mosfetF, LOW);
       delay(Time);  
       break;
     case 3: 
       digitalWrite(mosfetA, LOW);
-      digitalWrite(mosfetB, HIGH);
-      delay(Time);  
-      break;
-    case 4: 
-      digitalWrite(mosfetC, LOW);
       digitalWrite(mosfetD, HIGH);
       delay(Time);  
       break;
+    case 4: 
+      digitalWrite(mosfetB, LOW);
+      digitalWrite(mosfetE, HIGH);
+      delay(Time);  
+      break;
     case 5: 
-      digitalWrite(mosfetE, LOW);
+      digitalWrite(mosfetC, LOW);
       digitalWrite(mosfetF, HIGH);
       delay(Time);  
       break;
